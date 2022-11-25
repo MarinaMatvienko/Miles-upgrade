338d180d9ea86532c38d0a7423f326cbd4808e5d
Mary <mmatvienko111@gmail.com>
2022-11-25 10:34:37 +0300

    Change code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,7 @@ public class BonusMilesService {
 
     public int calculate(int cost) {
         int minInMile = 20;
-        int bonus = cost/minInMile;
+        int bonus = cost / minInMile;
         return bonus;
     }
 }
